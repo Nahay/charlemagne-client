ede059f42f82a4bbdab75a60b568ac3d54a11be8
Malek <rogex2542@gmail.com>
2022-03-08 20:36:47 +0100

    fix: wrong import

--- src/services/commandsListService.js
+++ src/services/commandsListService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { config } from './auth';
+import { config } from './config';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
